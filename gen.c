00e4ccfd8a687253a57ff13b3ffe9d4c91288001
Quentin Carbonneaux <qcarbonneaux@gmail.com>
2012-05-17 11:57:53 +0200

    Added code and term generation functions.

--- gen.c
+++ gen.c
@@ -21,16 +21,14 @@ struct Pat {
 /* struct PMat - A pattern matrix.
  */
 struct PMat {
-	struct {
-		struct Pat *ps;
-		struct Term *rhs;
-	} *m;
+	struct Pat **m;
+	struct Term **rhs;
 	int r, c;
 };
 
-/* MAXDEPTH - The maximum depth of a pattern.
+/* MAXPDPTH - The maximum depth of a pattern.
  */
-#define MAXDEPTH 32
+#define MAXPDPTH 32
 
 /* struct VPth - Associate a pattern variable to its path.
  * The path must be 0 terminated.
@@ -43,7 +41,7 @@ struct VPth {
 /* internal ploc - This stores the current path during the
  * construction of the initial pattern matrix.
  */
-static int ploc[MAXDEPTH];
+static int ploc[MAXPDPTH];
 
 /* internal re rvp - These two variables store the environment
  * and the bindings from pattern variables to paths of the
@@ -81,9 +79,9 @@ pat(int dpth, struct Pat *p, struct Term *t)
 {
 	int i;
 
-	if (dpth>=MAXDEPTH-1) {
+	if (dpth>=MAXPDPTH-1) {
 		fprintf(stderr, "%s: Maximum pattern depth exceeded.\n"
-		                "\tThe maximum is %d.\n", __func__, MAXDEPTH);
+		                "\tThe maximum is %d.\n", __func__, MAXPDPTH);
 		exit(1); // FIXME
 	}
 	if (t->typ==Var && eget(re, t->uvar)) {
@@ -118,15 +116,16 @@ pmnew(struct RSet *rs, struct VPth *vpa[])
 	m.r=rs->i;
 	m.c=rs->ar;
 	m.m=dkalloc(m.r*sizeof *m.m);
+	m.rhs=dkalloc(m.r*sizeof *m.rhs);
 
 	for (i=0; i<m.r; i++) {
-		m.m[i].ps=dkalloc(m.c*sizeof *m.m[i].ps);
-		m.m[i].rhs=rs->s[i].r;
+		m.m[i]=dkalloc(m.c*sizeof *m.m[i]);
+		m.rhs[i]=rs->s[i].r;
 		rvp=vpa[i];
 		re=rs->s[i].e;
 		for (n=m.c-1, t=rs->s[i].l; n>=0; n--, t=t->uapp.t1) {
 			ploc[0]=n;
-			pat(0, &m.m[i].ps[n], t->uapp.t2);
+			pat(0, &m.m[i][n], t->uapp.t2);
 		}
 	}
 	return m;
@@ -148,36 +147,43 @@ pmspec(struct PMat m, char *x, int ar, int c)
 
 	assert(c>=0 && c<m.c && m.r>0);
 	for (n.r=i=0; i<m.r; i++) {
-		if (m.m[i].ps[c].c==x || m.m[i].ps[c].ar<0)
+		if (m.m[i][c].c==x || m.m[i][c].ar<0)
 			n.r++;
 	}
 	n.c=m.c-1+ar;
-	if (n.r==0 || n.c==0) {
+	if (n.r==0) {
+		n.rhs=0;
+		n.m=0;
+		return n;
+	}
+	n.rhs=dkalloc(n.r*sizeof *n.m);
+	for (k=i=0; i<n.r; k++)
+		if (m.m[k][c].c==x || m.m[i][c].ar<0)
+			n.rhs[i++]=m.rhs[k];
+	if (n.c==0) {
 		n.m=0;
 		return n;
 	}
 	n.m=dkalloc(n.r*sizeof *n.m);
 
 	for (k=i=0; i<n.r; k++) {
-		struct Pat *ps=m.m[k].ps;
+		struct Pat *ps=m.m[k];
 		if (ps[c].c==x) {
-			n.m[i].ps=dkalloc(n.c*spat);
-			n.m[i].rhs=m.m[k].rhs;
-			memcpy(n.m[i].ps, ps, c*spat);
-			memcpy(n.m[i].ps+c, ps[c].ps, ar*spat);
-			memcpy(n.m[i].ps+c+ar, ps+c+1, (n.c-c-ar)*spat);
+			n.m[i]=dkalloc(n.c*spat);
+			memcpy(n.m[i], ps, c*spat);
+			memcpy(n.m[i]+c, ps[c].ps, ar*spat);
+			memcpy(n.m[i]+c+ar, ps+c+1, (n.c-c-ar)*spat);
 			i++;
 		}
 		else if (ps[c].ar<0) {
-			n.m[i].ps=dkalloc(n.c*spat);
-			n.m[i].rhs=m.m[k].rhs;
-			memcpy(n.m[i].ps, ps, c*spat);
+			n.m[i]=dkalloc(n.c*spat);
+			memcpy(n.m[i], ps, c*spat);
 			for (j=0; j<ar; j++) {
-				n.m[i].ps[c+j].c=0;
-				n.m[i].ps[c+j].ar=-1;
-				n.m[i].ps[c+j].ps=0;
+				n.m[i][c+j].c=0;
+				n.m[i][c+j].ar=-1;
+				n.m[i][c+j].ps=0;
 			}
-			memcpy(n.m[i].ps+c+ar, ps+c+1, (n.c-c-ar)*spat);
+			memcpy(n.m[i]+c+ar, ps+c+1, (n.c-c-ar)*spat);
 			i++;
 		}
 	}
@@ -197,20 +203,28 @@ pmdef(struct PMat m, int c)
 
 	assert(c<m.c);
 	for (n.r=0, i=0; i<m.r; i++)
-		if (m.m[i].ps[c].ar<0)
+		if (m.m[i][c].ar<0)
 			n.r++;
 	n.c=m.c-1;
-	if (n.c==0 || n.r==0) {
+	if (n.r==0) {
+		n.rhs=0;
+		n.m=0;
+		return n;
+	}
+	n.rhs=dkalloc(n.r*sizeof *n.m);
+	for (k=i=0; i<n.r; k++)
+		if (m.m[i][c].ar<0)
+			n.rhs[i++]=m.rhs[k];
+	if (n.c==0) {
 		n.m=0;
 		return n;
 	}
 	n.m=dkalloc(n.r*sizeof *n.m);
 
 	for (k=i=0; i<n.r; k++)
-		if (m.m[k].ps[c].ar<0) {
-			n.m[i].ps=dkalloc(n.c*spat);
-			n.m[i].rhs=m.m[k].rhs;
-			memcpy(n.m[i].ps, m.m[k].ps+1, n.c*spat);
+		if (m.m[k][c].ar<0) {
+			n.m[i]=dkalloc(n.c*spat);
+			memcpy(n.m[i], m.m[k]+1, n.c*spat);
 			i++;
 		}
 	return n;
@@ -226,6 +240,16 @@ pmdef(struct PMat m, int c)
  * representation.
  */
 
+/* enum NameType - Specify the name type that must be generated
+ * for a variable, C means 'Code' and T means 'Term'. This is
+ * used to call the gname function.
+ */
+enum NameType { C, T };
+
+static char *gname(enum NameType, char *);
+static void gterm(struct Term *);
+static void gcode(struct Term *);
+
 /* ------------- Rule set compiling. ------------- */
 
 /* internal comprs - Compile a rule set following a pattern
@@ -238,34 +262,39 @@ comprs(struct PMat pm)
 {
 	int i, c;
 	struct Pat *p;
+	struct PMat m;
 
 	if (pm.r==0) {
-		emit("fail\n");
+		emit("--[[ FIXME ]]");
 		return;
 	}
 	for (c=0; c<pm.c; c++)
-		if (pm.m[0].ps[c].ar>=0)
+		if (pm.m[0][c].ar>=0)
 			break;
 	if (c==pm.c) {
-		emit("sucess\n");
+		emit("return ");
+		gcode(pm.rhs[0]);
 		return;
 	}
 
-	p=&pm.m[0].ps[c];
+	p=&pm.m[0][c];
 	emit("if %s then\n", p->c);
-	comprs(pmspec(pm, p->c, p->ar, c));
+	m=pmspec(pm, p->c, p->ar, c);
+	comprs(m);
 
 	for (i=1; i<pm.r; i++) {
-		p=&pm.m[i].ps[c];
+		p=&pm.m[i][c];
 		if (p->ar>=0) {
-			emit("elseif %s then\n", p->c);
-			comprs(pmspec(pm, p->c, p->ar, c));
+			emit("\nelseif %s then\n", p->c);
+			m=pmspec(pm, p->c, p->ar, c);
+			comprs(m);
 		}
 	}
 
-	emit("else\n");
-	comprs(pmdef(pm, c));
-	emit("end\n");
+	emit("\nelse\n");
+	m=pmdef(pm, c);
+	comprs(m);
+	emit("\nend\n");
 }
 
 /* internal fillvpa - This function will fill the struct VPth object
@@ -303,7 +332,7 @@ genrules(struct RSet *rs)
 	}
 
 	if (rs->ar>0) {
-		emit("{- Compiling rules for %s. -}\n", rs->x);
+		emit("--[[ Compiling rules for %s. ]]\n", rs->x);
 		pm=pmnew(rs, vpa);
 		comprs(pm);
 		emit("\n");
@@ -314,3 +343,132 @@ genrules(struct RSet *rs)
 /* ------------- Declaration compiling. ------------- */
 
 /* ------------- Term compiling. ------------- */
+
+/* MAXSDPTH - The maximum stack depth used in functions below,
+ * these stacks are used to recurse on terms.
+ */
+#define MAXSDPTH 128
+
+/* MAXID - Maximum size of an identifier in the generated code.
+ */
+#define MAXID 128
+
+/* internal gname - Generate the variable name to be emitted to
+ * access a variable. This name can be of two kinds, either a
+ * 'code' name or a 'term' name.
+ * Warning, this returns a static buffer.
+ */
+static inline char *
+gname(enum NameType nt, char *x)
+{
+	static char s[MAXID];
+
+	switch (nt) {
+	case C:
+		snprintf(s, MAXID, "%s_c", x);
+		break;
+	case T:
+		snprintf(s, MAXID, "%s_t", x);
+		break;
+	}
+	return s;
+}
+
+static void
+gcode(struct Term *t)
+{
+	char **lams;
+	int i, s;
+
+/* tail: */
+	switch (t->typ) {
+	case Var:
+		emit("%s", gname(C, t->uvar));
+		break;
+	case Lam:
+		lams=xalloc(MAXSDPTH*sizeof *lams);
+		for (s=0; t->typ==Lam; s++) {
+			if (s>=MAXSDPTH) {
+				fprintf(stderr, "%s: Maximum recursion depth exceeded.\n"
+				                "\tThe maximum is %d.\n", __func__, MAXSDPTH);
+				exit(1); // FIXME
+			}
+			lams[s]=t->ulam.x;
+			t=t->ulam.t;
+		}
+		emit("{ ck = clam, arity = %d, args = {}, clam = function (", s);
+		for (i=0; i<s-1; i++)
+			emit("%s, ", gname(C, lams[i]));
+		emit("%s) return ", gname(C, lams[i]));
+		free(lams);
+		gcode(t);
+		emit(" end }");
+		break;
+	case Pi:
+		emit("{ ck = cpi, cpi = { ");
+		gcode(t->upi.ty);
+		emit(", function (%s) return ",
+		     t->upi.x ? gname(C, t->upi.x) : "dkhole");
+		gcode(t->upi.t);
+		emit(" end } }");
+		break;
+	case App:
+		emit("ap(");
+		gcode(t->uapp.t1);
+		emit(", ");
+		gcode(t->uapp.t2);
+		emit(")");
+		break;
+	case Type:
+		emit("{ ck = ctype }");
+		break;
+	}
+}
+
+static void
+gterm(struct Term *t)
+{
+	char *x;
+/* tail: */
+	switch (t->typ) {
+	case Var:
+		emit("%s", gname(T, t->uvar));
+		break;
+	case Lam:
+		emit("{ tk = tlam, tlam = { nil, ");
+		emit("function (%s, ", gname(T, t->ulam.x));
+		emit(", %s) return ", gname(C, t->ulam.x));
+		gterm(t->ulam.t);
+		emit(" end } }");
+		break;
+	case Pi:
+		emit("{ tk = tpi; tpi = { ");
+		if (t->upi.ty->typ==Var)
+			emit(gname(C, t->upi.ty->uvar));
+		else {
+			emit("chkabs(");
+			gterm(t->upi.ty);
+			emit(", ");
+			gcode(t->upi.ty);
+			emit(")");
+		}
+		x = t->upi.x ? t->upi.x : "dkhole";
+		emit(", function (%s, ", gname(T, x));
+		emit("%s) return ", gname(C, x));
+		gterm(t->upi.t);
+		emit(" end } }");
+		break;
+	case App:
+		emit("{ tk = tapp, tapp = { ");
+		gterm(t->uapp.t1);
+		emit(", ");
+		gterm(t->uapp.t2);
+		emit(", ");
+		gcode(t->uapp.t2);
+		emit(" } }");
+		break;
+	case Type:
+		emit("{ tk = ttype }");
+		break;
+	}
+}
